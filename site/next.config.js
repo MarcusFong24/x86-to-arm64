f720d85b51844520dd4d8513daae9f7a1879a61c
MarcusFong24 <129456340+MarcusFong24@users.noreply.github.com>
2023-10-07 15:28:52 -0500

    next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'X86 to Arm64',
+    description: 'LavaFlows's X86 to Arm64 Registry',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
